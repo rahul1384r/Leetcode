de73c69858a7f00c964198e79d0873f4ceaec8bb
rahul1384r <156775643+rahul1384r@users.noreply.github.com>
2024-01-19 22:22:08 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,7 +6,7 @@ public class Main
         for(int i=0;i<numbers.length;i++){
             if(numbers[i]==key) {
                 return i;
-            }
+			}
         }
         return -1;
     } 
