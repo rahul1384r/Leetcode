cc1676f8f74b6232722533140656b72e5e7dcc93
rahul1384r <156775643+rahul1384r@users.noreply.github.com>
2024-01-23 13:59:41 +0530

    revised array searches

---
+++ check.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class check {
+    public static void main(String[] args) {
+        int arr[]={1,2,3,4,5};
+        for(int i=0;i<arr.length;i++){
+            for(int j=i+1;j<arr.length;j++){
+                for(int k=i;k<j;k++){
+                    System.out.println();
+                }
+            }
+        }
+        
+        
+        
+    }
+}
