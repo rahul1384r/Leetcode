7959626247277151f7878669c7363ac70a715f47
rahul1384r <156775643+rahul1384r@users.noreply.github.com>
2024-02-07 16:55:13 +0530

    s

--- Main.java
+++ Main.java
@@ -1,24 +1,14 @@
 import java.util.*;
-//Linear Search
-public class Main
-{
-    public static int LinearSearch(int numbers[],int key){
-        for(int i=0;i<numbers.length;i++){
-            if(numbers[i]==key) {
-                return i;
-			}
-        }
-        return -1;
-    } 
+public class Main{
 	public static void main(String[] args) {
-	    int numbers[]={2,3,4,5,6,7,8,55,75,678};
-	    int key=6;
-	    int index = LinearSearch(numbers,key);
-	    if(index == -1){
-	        System.out.println("NOT FOUND");
-	    }
-	    else {
-	    System.out.println("key is at index: "+ index);
+		String str="mississipp";
+		String sub="issip";
+		for(int i=0;i<str.length();i++){
+			for(int j=0;j<sub.length();j++){
+				if(){
+					System.out.println(i);
+				}
+			}
+		}
 	}
-}
 }
---
+++ length_of_lastWord.java
@@ -0,0 +1,5 @@
+public class length_of_lastWord {
+    public static void main(String[] args) {
+        
+    }
+}
