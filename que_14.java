b0c56fc8e5bc91a82d25a5636a065ac9d9c36e93
rahul1384r <156775643+rahul1384r@users.noreply.github.com>
2024-02-07 16:30:12 +0530

    que 14 of leetcode

---
+++ que_14.java
@@ -0,0 +1,11 @@
+public class que_14 {
+    public static void main(String[] args) {
+        String arr[]={"flower","RRY"};
+        for(int i=0;i<arr.length;i++){
+            String a=arr[i];
+            for(int j=0;j<a.length();j++){
+                System.out.println(a.charAt(j));
+            }
+        }
+    }
+}
