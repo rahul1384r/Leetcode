c181c329d7ad5965b813eed4d2b94950e9ea188d
rahul1384r <156775643+rahul1384r@users.noreply.github.com>
2024-02-29 00:42:21 +0530

    s

--- que_35.java
+++ que_35.java
@@ -1,6 +1,6 @@
 public class que_35 {
     public static void main(String[] args) {
-        int arr[]={1,3,5,6};
+        int arr[]={1,3,5,2,5,8,3};
         int target=2;
         int start=0;
         int end=arr.length-1;
