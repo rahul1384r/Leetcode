d4638d602a581bb3fd3d3d5df332d218bf57e617
rahul1384r <156775643+rahul1384r@users.noreply.github.com>
2024-03-19 15:50:02 +0530

    s

---
+++ que_189.java
@@ -0,0 +1,10 @@
+public class que_189 {
+    public static void main(String[] args) {
+        int arr[]={1,2,3,4,5,6,7};
+        int k=3;
+        int ans[]=new int[k];
+        for(int i=0;i<k;i++){
+            
+        }
+    }
+}
