1aed07c2b6a2e53e3e6f8eab0374284fb9b6a5cc
rahul1384r <156775643+rahul1384r@users.noreply.github.com>
2024-03-29 11:34:29 +0530

    s

---
+++ que_11.java
@@ -0,0 +1,12 @@
+public class que_11 {
+    public static void main(String[] args) {
+        int arr[]={1,8,6,2,5,4,8,3,7};
+        int max=0;
+
+        for(int i=0;i<arr.length;i++){
+            for(int j=i+1;j<arr.length;j++){
+                max=arr[i]*arr[j];
+            }
+        }
+    }
+}
---
+++ que_31.java
@@ -0,0 +1,13 @@
+public class que_31 {
+    public static void main(String[] args) {
+        int arr[]={1,2,3};
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]<arr[i+1]){
+                break;
+            }
+            else{
+                
+            }
+        }
+    }
+}
