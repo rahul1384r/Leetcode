0dcaebc9a5aafa27936e18e97a9900eb12325b75
rahul1384r <156775643+rahul1384r@users.noreply.github.com>
2024-04-15 19:52:06 +0530

    s

---
+++ Test.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Test {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        Integer.toString(n);
+        System.out.println(n);
+    }
+}
